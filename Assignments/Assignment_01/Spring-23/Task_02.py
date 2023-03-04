0b65b4da5d93e069f41e2c855cedf31a815d2885
Ibrahim Alif <100870354+fluidpotata@users.noreply.github.com>
2023-03-04 22:06:19 +0600

    Rotate Left k cell

---
+++ Assignments/Assignment_01/Spring-23/Task_02.py
@@ -0,0 +1,15 @@
+# Test 02: Rotate Left k cell
+def rotate_left(source, k):
+
+  # Hint, You can write a function for left rotate once and then use it
+  length = len(source)
+
+  for j in range(k):   
+    tmp = source[0]
+    for i in range(len(source)-1):
+        source[i] = source[i+1]
+
+    source[len(source)-1] = tmp
+
+  return source
+
